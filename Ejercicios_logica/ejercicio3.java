d482bfdadd52748a3c0eb9ae7c1da9c26cfabfc3
Euder <ejpachecoa@ufpso.edu.co>
2024-10-05 16:32:12 -0500

    (doc)Readme:HICE EL EJERCICIO

---
+++ Ejercicios_logica/ejercicio3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ejercicio3 {
+    public static void main(String[] args)  {
+  Scanner scanner=new  Scanner(System.in);
+  System.out.println("ingrese 3 numeros para calcular primedio");
+  int numero=[3];
+  double promedio=numero
+    }  
+} 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Parcial-I-192356
+ejercicio1
+para poder ingresar los numero de las dimendiones de los poligonos utilizare el scanner 
+utilizarè el switch case para poder escoger las opciones 
+colocare los procesos dentro delos case 
+ejercicio3
+hago dos arrays para los valores
